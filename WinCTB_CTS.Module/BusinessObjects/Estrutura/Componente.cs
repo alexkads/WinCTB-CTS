24a8edef0ddeb0d6520b574e6ebda2b18737cce6
alexkads <alexkads@gmail.com>
2021-01-23 20:05:17 -0300

    Criação de componete e junta de estrutura

---
+++ WinCTB_CTS.Module/BusinessObjects/Estrutura/Componente.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace WinCTB_CTS.Module.BusinessObjects.Estrutura
+{
+    [DefaultClassOptions]
+    public class Componente : BaseObject
+    { 
+        public Componente(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+        }
+    }
+}
