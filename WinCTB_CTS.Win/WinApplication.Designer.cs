dadee357935cd871658d15c4dd1b2e3ebba1d091
alexkads <alexkads@gmail.com>
2021-01-23 09:30:56 -0300

    Primeiro Atualização

---
+++ WinCTB_CTS.Win/WinApplication.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WinCTB_CTS.Win {
+    partial class WinCTB_CTSWindowsFormsApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new WinCTB_CTS.Module.WinCTB_CTSModule();
+            this.module4 = new WinCTB_CTS.Module.Win.WinCTB_CTSWindowsFormsModule();
+            this.auditTrailModule = new DevExpress.ExpressApp.AuditTrail.AuditTrailModule();
+            this.objectsModule = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            this.chartModule = new DevExpress.ExpressApp.Chart.ChartModule();
+            this.chartWindowsFormsModule = new DevExpress.ExpressApp.Chart.Win.ChartWindowsFormsModule();
+            this.cloneObjectModule = new DevExpress.ExpressApp.CloneObject.CloneObjectModule();
+            this.conditionalAppearanceModule = new DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule();
+            this.dashboardsModule = new DevExpress.ExpressApp.Dashboards.DashboardsModule();
+            this.dashboardsWindowsFormsModule = new DevExpress.ExpressApp.Dashboards.Win.DashboardsWindowsFormsModule();
+            this.fileAttachmentsWindowsFormsModule = new DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule();
+            this.kpiModule = new DevExpress.ExpressApp.Kpi.KpiModule();
+            this.officeWindowsFormsModule = new DevExpress.ExpressApp.Office.Win.OfficeWindowsFormsModule();
+            this.pivotChartModuleBase = new DevExpress.ExpressApp.PivotChart.PivotChartModuleBase();
+            this.pivotChartWindowsFormsModule = new DevExpress.ExpressApp.PivotChart.Win.PivotChartWindowsFormsModule();
+            this.pivotGridModule = new DevExpress.ExpressApp.PivotGrid.PivotGridModule();
+            this.pivotGridWindowsFormsModule = new DevExpress.ExpressApp.PivotGrid.Win.PivotGridWindowsFormsModule();
+            this.reportsModuleV2 = new DevExpress.ExpressApp.ReportsV2.ReportsModuleV2();
+            this.reportsWindowsFormsModuleV2 = new DevExpress.ExpressApp.ReportsV2.Win.ReportsWindowsFormsModuleV2();
+            this.treeListEditorsModuleBase = new DevExpress.ExpressApp.TreeListEditors.TreeListEditorsModuleBase();
+            this.treeListEditorsWindowsFormsModule = new DevExpress.ExpressApp.TreeListEditors.Win.TreeListEditorsWindowsFormsModule();
+            this.validationModule = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.validationWindowsFormsModule = new DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule();
+            this.viewVariantsModule = new DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            //
+            // auditTrailModule
+            //
+            this.auditTrailModule.AuditDataItemPersistentType = typeof(DevExpress.Persistent.BaseImpl.AuditDataItemPersistent);
+            //
+            // dashboardsModule
+            //
+            this.dashboardsModule.DashboardDataType = typeof(DevExpress.Persistent.BaseImpl.DashboardData);
+            this.dashboardsWindowsFormsModule.DesignerFormStyle = DevExpress.XtraBars.Ribbon.RibbonFormStyle.Ribbon;
+            //
+            // reportsModuleV2
+            //
+            this.reportsModuleV2.EnableInplaceReports = true;
+            this.reportsModuleV2.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.ReportDataV2);
+            this.reportsModuleV2.ShowAdditionalNavigation = false;
+            this.reportsModuleV2.ReportStoreMode = DevExpress.ExpressApp.ReportsV2.ReportStoreModes.XML;
+            // 
+            // WinCTB_CTSWindowsFormsApplication
+            // 
+            this.ApplicationName = "WinCTB-CTS";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.auditTrailModule);
+            this.Modules.Add(this.objectsModule);
+            this.Modules.Add(this.chartModule);
+            this.Modules.Add(this.chartWindowsFormsModule);
+            this.Modules.Add(this.cloneObjectModule);
+            this.Modules.Add(this.conditionalAppearanceModule);
+            this.Modules.Add(this.dashboardsModule);
+            this.Modules.Add(this.dashboardsWindowsFormsModule);
+            this.Modules.Add(this.fileAttachmentsWindowsFormsModule);
+            this.Modules.Add(this.kpiModule);
+            this.Modules.Add(this.officeWindowsFormsModule);
+            this.Modules.Add(this.pivotChartModuleBase);
+            this.Modules.Add(this.pivotChartWindowsFormsModule);
+            this.Modules.Add(this.pivotGridModule);
+            this.Modules.Add(this.pivotGridWindowsFormsModule);
+            this.Modules.Add(this.reportsModuleV2);
+            this.Modules.Add(this.reportsWindowsFormsModuleV2);
+            this.Modules.Add(this.treeListEditorsModuleBase);
+            this.Modules.Add(this.treeListEditorsWindowsFormsModule);
+            this.Modules.Add(this.validationModule);
+            this.Modules.Add(this.validationWindowsFormsModule);
+            this.Modules.Add(this.viewVariantsModule);
+            this.UseOldTemplates = false;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.WinCTB_CTSWindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.WinCTB_CTSWindowsFormsApplication_CustomizeLanguagesList);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private WinCTB_CTS.Module.WinCTB_CTSModule module3;
+        private WinCTB_CTS.Module.Win.WinCTB_CTSWindowsFormsModule module4;
+        private DevExpress.ExpressApp.AuditTrail.AuditTrailModule auditTrailModule;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule objectsModule;
+        private DevExpress.ExpressApp.Chart.ChartModule chartModule;
+        private DevExpress.ExpressApp.Chart.Win.ChartWindowsFormsModule chartWindowsFormsModule;
+        private DevExpress.ExpressApp.CloneObject.CloneObjectModule cloneObjectModule;
+        private DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule conditionalAppearanceModule;
+        private DevExpress.ExpressApp.Dashboards.DashboardsModule dashboardsModule;
+        private DevExpress.ExpressApp.Dashboards.Win.DashboardsWindowsFormsModule dashboardsWindowsFormsModule;
+        private DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule fileAttachmentsWindowsFormsModule;
+        private DevExpress.ExpressApp.Kpi.KpiModule kpiModule;
+        private DevExpress.ExpressApp.Office.Win.OfficeWindowsFormsModule officeWindowsFormsModule;
+        private DevExpress.ExpressApp.PivotChart.PivotChartModuleBase pivotChartModuleBase;
+        private DevExpress.ExpressApp.PivotChart.Win.PivotChartWindowsFormsModule pivotChartWindowsFormsModule;
+        private DevExpress.ExpressApp.PivotGrid.PivotGridModule pivotGridModule;
+        private DevExpress.ExpressApp.PivotGrid.Win.PivotGridWindowsFormsModule pivotGridWindowsFormsModule;
+        private DevExpress.ExpressApp.ReportsV2.ReportsModuleV2 reportsModuleV2;
+        private DevExpress.ExpressApp.ReportsV2.Win.ReportsWindowsFormsModuleV2 reportsWindowsFormsModuleV2;
+        private DevExpress.ExpressApp.TreeListEditors.TreeListEditorsModuleBase treeListEditorsModuleBase;
+        private DevExpress.ExpressApp.TreeListEditors.Win.TreeListEditorsWindowsFormsModule treeListEditorsWindowsFormsModule;
+        private DevExpress.ExpressApp.Validation.ValidationModule validationModule;
+        private DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule validationWindowsFormsModule;
+        private DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule viewVariantsModule;
+    }
+}
