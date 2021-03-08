3c4fb6c3ce95573cfa2910b3b60739b6bd635d6a
Alex da Silva <alexkads@gmail.com>
2021-03-08 20:38:49 -0300

    Criado model para medição detalhada de Estrutura

--- WinCTB_CTS.Module/BusinessObjects/Estrutura/Medicao/MedicaoEstruturaDetalhe.cs
+++ WinCTB_CTS.Module/BusinessObjects/Estrutura/Medicao/MedicaoEstruturaDetalhe.cs
@@ -12,41 +12,253 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 
-namespace WinCTB_CTS.Module.BusinessObjects.Estrutura.Medicao
-{
-    public class MedicaoEstruturaDetalhe : BaseObject
-    { 
+namespace WinCTB_CTS.Module.BusinessObjects.Estrutura.Medicao {
+    public class MedicaoEstruturaDetalhe : BaseObject {
         public MedicaoEstruturaDetalhe(Session session)
-            : base(session)
-        {
+            : base(session) {
         }
-        public override void AfterConstruction()
-        {
+        public override void AfterConstruction() {
             base.AfterConstruction();
         }
 
+        double percAvancoTotalPoderado;
+        double pesoAvancoTotalPoderado;
+        double eAPPesoEND;
+        double eAPPesoSolda;
+        double eAPPesoFitUP;
+        double eAPPesoPosicionamento;
+        double pesoEND;
+        double pesoRX;
+        double pesoUS;
+        double pesoLPPM;
+        double pesoVisual;
+        double pesoSolda;
+        double pesoFitUp;
+        double pesoPosicionamento;
+        double percAvancoEND;
+        double percAvancoRX;
+        double percAvancoUS;
+        double percAvancoLPPM;
+        double percAvancoVisual;
+        double percAvancoSolda;
+        double percAvancoFitUp;
+        double percPosicionamento;
+        double eNDExecutaMM;
+        double eNDPrevistoMM;
+        double rXExecutadoMM;
+        double rXPrevistoMM;
+        double uSExecutadoMM;
+        double uSPrevistoMM;
+        double lPPMExecutadoMM;
+        double lPPMPrevistoMM;
+        double visualExecutadoMM;
+        double visualPrevistoMM;
+        double soldaExecutadoMM;
+        double fitUpExecutadoMM;
+        double medJointMM;
         double pesoTotal;
         private Componente componente;
         private MedicaoEstrutura medicaoEstrutura;
 
         [Association("MedicaoEstrutura-MedicaoEstruturaDetalhes")]
-        public MedicaoEstrutura MedicaoEstrutura
-        {
+        public MedicaoEstrutura MedicaoEstrutura {
             get => medicaoEstrutura;
             set => SetPropertyValue(nameof(MedicaoEstrutura), ref medicaoEstrutura, value);
         }
 
         [Association("Componente-MedicaoEstruturaDetalhes")]
-        public Componente Componente
-        {
+        public Componente Componente {
             get => componente;
             set => SetPropertyValue(nameof(Componente), ref componente, value);
         }
-               
-        public double PesoTotal
-        {
+
+        public double PesoTotal {
             get => pesoTotal;
             set => SetPropertyValue(nameof(PesoTotal), ref pesoTotal, value);
+        }
+
+        public double MedJointMM {
+            get => medJointMM;
+            set => SetPropertyValue(nameof(MedJointMM), ref medJointMM, value);
+        }
+
+        public double FitUpExecutadoMM {
+            get => fitUpExecutadoMM;
+            set => SetPropertyValue(nameof(FitUpExecutadoMM), ref fitUpExecutadoMM, value);
+        }
+
+        public double SoldaExecutadoMM {
+            get => soldaExecutadoMM;
+            set => SetPropertyValue(nameof(SoldaExecutadoMM), ref soldaExecutadoMM, value);
+        }
+
+        public double VisualPrevistoMM {
+            get => visualPrevistoMM;
+            set => SetPropertyValue(nameof(VisualPrevistoMM), ref visualPrevistoMM, value);
+        }
+
+        public double VisualExecutadoMM {
+            get => visualExecutadoMM;
+            set => SetPropertyValue(nameof(VisualExecutadoMM), ref visualExecutadoMM, value);
+        }
+
+        public double LPPMPrevistoMM {
+            get => lPPMPrevistoMM;
+            set => SetPropertyValue(nameof(LPPMPrevistoMM), ref lPPMPrevistoMM, value);
+        }
+
+        public double LPPMExecutadoMM {
+            get => lPPMExecutadoMM;
+            set => SetPropertyValue(nameof(LPPMExecutadoMM), ref lPPMExecutadoMM, value);
+        }
+
+        public double USPrevistoMM {
+            get => uSPrevistoMM;
+            set => SetPropertyValue(nameof(USPrevistoMM), ref uSPrevistoMM, value);
+        }
+
+        public double USExecutadoMM {
+            get => uSExecutadoMM;
+            set => SetPropertyValue(nameof(USExecutadoMM), ref uSExecutadoMM, value);
+        }
+
+        public double RXPrevistoMM {
+            get => rXPrevistoMM;
+            set => SetPropertyValue(nameof(RXPrevistoMM), ref rXPrevistoMM, value);
+        }
+
+        public double RXExecutadoMM {
+            get => rXExecutadoMM;
+            set => SetPropertyValue(nameof(RXExecutadoMM), ref rXExecutadoMM, value);
+        }
+
+        public double ENDPrevistoMM {
+            get => eNDPrevistoMM;
+            set => SetPropertyValue(nameof(ENDPrevistoMM), ref eNDPrevistoMM, value);
+        }
+
+        public double ENDExecutaMM {
+            get => eNDExecutaMM;
+            set => SetPropertyValue(nameof(ENDExecutaMM), ref eNDExecutaMM, value);
+        }
+
+        [ModelDefault("DisplayFormat", "P4"), ModelDefault("EditMask", "P4")]
+        public double PercPosicionamento {
+            get => percPosicionamento;
+            set => SetPropertyValue(nameof(PercPosicionamento), ref percPosicionamento, value);
+        }
+
+        [ModelDefault("DisplayFormat", "P4"), ModelDefault("EditMask", "P4")]
+        public double PercAvancoFitUp {
+            get => percAvancoFitUp;
+            set => SetPropertyValue(nameof(PercAvancoFitUp), ref percAvancoFitUp, value);
+        }
+
+        [ModelDefault("DisplayFormat", "P4"), ModelDefault("EditMask", "P4")]
+        public double PercAvancoSolda {
+            get => percAvancoSolda;
+            set => SetPropertyValue(nameof(PercAvancoSolda), ref percAvancoSolda, value);
+        }
+
+        [ModelDefault("DisplayFormat", "P4"), ModelDefault("EditMask", "P4")]
+        public double PercAvancoVisual {
+            get => percAvancoVisual;
+            set => SetPropertyValue(nameof(PercAvancoVisual), ref percAvancoVisual, value);
+        }
+
+        [ModelDefault("DisplayFormat", "P4"), ModelDefault("EditMask", "P4")]
+        public double PercAvancoLPPM {
+            get => percAvancoLPPM;
+            set => SetPropertyValue(nameof(PercAvancoLPPM), ref percAvancoLPPM, value);
+        }
+
+        [ModelDefault("DisplayFormat", "P4"), ModelDefault("EditMask", "P4")]
+        public double PercAvancoUS {
+            get => percAvancoUS;
+            set => SetPropertyValue(nameof(PercAvancoUS), ref percAvancoUS, value);
+        }
+
+        [ModelDefault("DisplayFormat", "P4"), ModelDefault("EditMask", "P4")]
+        public double PercAvancoRX {
+            get => percAvancoRX;
+            set => SetPropertyValue(nameof(PercAvancoRX), ref percAvancoRX, value);
+        }
+
+        [ModelDefault("DisplayFormat", "P4"), ModelDefault("EditMask", "P4")]
+        public double PercAvancoEND {
+            get => percAvancoEND;
+            set => SetPropertyValue(nameof(PercAvancoEND), ref percAvancoEND, value);
+        }
+
+        public double PesoPosicionamento {
+            get => pesoPosicionamento;
+            set => SetPropertyValue(nameof(PesoPosicionamento), ref pesoPosicionamento, value);
+        }
+
+        public double PesoFitUp {
+            get => pesoFitUp;
+            set => SetPropertyValue(nameof(PesoFitUp), ref pesoFitUp, value);
+        }
+
+        public double PesoSolda {
+            get => pesoSolda;
+            set => SetPropertyValue(nameof(PesoSolda), ref pesoSolda, value);
+        }
+
+        public double PesoVisual {
+            get => pesoVisual;
+            set => SetPropertyValue(nameof(PesoVisual), ref pesoVisual, value);
+        }
+
+        public double PesoLPPM {
+            get => pesoLPPM;
+            set => SetPropertyValue(nameof(PesoLPPM), ref pesoLPPM, value);
+        }
+
+        public double PesoUS {
+            get => pesoUS;
+            set => SetPropertyValue(nameof(PesoUS), ref pesoUS, value);
+        }
+
+        public double PesoRX {
+            get => pesoRX;
+            set => SetPropertyValue(nameof(PesoRX), ref pesoRX, value);
+        }
+
+        public double PesoEND {
+            get => pesoEND;
+            set => SetPropertyValue(nameof(PesoEND), ref pesoEND, value);
+        }
+
+        public double EAPPesoPosicionamento {
+            get => eAPPesoPosicionamento;
+            set => SetPropertyValue(nameof(EAPPesoPosicionamento), ref eAPPesoPosicionamento, value);
+        }
+
+        public double EAPPesoFitUP {
+            get => eAPPesoFitUP;
+            set => SetPropertyValue(nameof(EAPPesoFitUP), ref eAPPesoFitUP, value);
+        }
+
+        public double EAPPesoSolda {
+            get => eAPPesoSolda;
+            set => SetPropertyValue(nameof(EAPPesoSolda), ref eAPPesoSolda, value);
+        }
+
+        public double EAPPesoEND {
+            get => eAPPesoEND;
+            set => SetPropertyValue(nameof(EAPPesoEND), ref eAPPesoEND, value);
+        }
+
+        public double PesoAvancoTotalPoderado {
+            get => pesoAvancoTotalPoderado;
+            set => SetPropertyValue(nameof(PesoAvancoTotalPoderado), ref pesoAvancoTotalPoderado, value);
+        }
+
+        [ModelDefault("DisplayFormat", "P4"), ModelDefault("EditMask", "P4")]
+        public double PercAvancoTotalPoderado {
+            get => percAvancoTotalPoderado;
+            set => SetPropertyValue(nameof(PercAvancoTotalPoderado), ref percAvancoTotalPoderado, value);
         }
     }
 }
